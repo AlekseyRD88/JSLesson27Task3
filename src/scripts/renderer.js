4d767df689b16c7229c3d6424c3f3bc0bbde540a
= <neymegen50@gmail.com>
2022-04-27 21:44:39 +0300

    message1

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -1,45 +1,12 @@
-/*
+import { getItem, setItem } from './storage';
+
 const listElem = document.querySelector('.list');
-
-
-const tasks = [
-  { text: 'Buy milk', done: false, id: '1' },
-  { text: 'Pick up Tom from airport', done: false, id: '2' },
-  { text: 'Visit party', done: false, id: '3' },
-  { text: 'Visit doctor', done: true, id: '4' },
-  { text: 'Buy meat', done: true, id: '5' },
-];
-
-const renderTasks = (tasksList) => {
-  listElem.innerHTML = '';
-  const tasksElems = tasksList
-    .slice()
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done, id }) => {
-      const listItemElem = document.createElement('li'
-      listItemElem.classList.add('list__item');
-      const checkbox = document.createElement('input');
-      checkbox.setAttribute('type', 'checkbox');
-      checkbox.setAttribute('data-id', id);
-      checkbox.checked = done;
-      checkbox.classList.add('list__item-checkbox');
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      listItemElem.append(checkbox, text);
-
-      return listItemElem;
-    });
-
-  listElem.append(...tasksElems);
-};
-renderTasks(tasks);*/
 const createCheckbox = ({ done, id }) => {
   const checkboxElem = document.createElement('input');
-  checkbox.setAttribute('type', 'checkbox');
-  checkbox.setAttribute('data-id', id);
-  checkbox.checked = done;
-  checkbox.classList.add('list__item-checkbox');
+  checkboxElem.setAttribute('type', 'checkbox');
+  checkboxElem.setAttribute('data-id', id);
+  checkboxElem.checked = done;
+  checkboxElem.classList.add('list__item-checkbox');
   return checkboxElem;
 };
 const createListItem = ({ text, done, id }) => {
@@ -49,7 +16,7 @@ const createListItem = ({ text, done, id }) => {
   if (done) {
     listItemElem.classList.add('list__item_done');
   }
-  listItemElem.append(checkbox, text);
+  listItemElem.append(checkboxElem, text);
   return listItemElem;
 };
 export const renderTasks = () => {
--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -1,4 +1,4 @@
-const storage = {};
+//const storage = {};
 export const setItem = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
   //Object.assign(storage, { [key]: value });
